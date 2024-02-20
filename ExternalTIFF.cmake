3f4618cb6c02673580efdb139f27a93e53954006
Erik Smistad <ersmistad@gmail.com>
2024-02-20 09:30:35 +0100

    Tried apple build for tiff

--- ExternalTIFF.cmake
+++ ExternalTIFF.cmake
@@ -24,6 +24,33 @@ ExternalProject_Add(tiff
           -DCMAKE_INSTALL_MESSAGE:BOOL=LAZY
 	  -DCMAKE_INSTALL_PREFIX:STRING=${INSTALL_DIR}
         )
+elseif(APPLE)
+create_package_code("
+	file(COPY ${INSTALL_DIR}/include/ DESTINATION ${POST_INSTALL_DIR}/include/)
+	file(COPY ${INSTALL_DIR}/lib/libtiff.dylib DESTINATION ${POST_INSTALL_DIR}/lib/ FOLLOW_SYMLINK_CHAIN)
+	file(COPY ${SOURCE_DIR}/COPYRIGHT DESTINATION ${POST_INSTALL_DIR}/licenses/${NAME}/)
+")
+ExternalProject_Add(tiff
+        PREFIX ${BUILD_DIR}
+        BINARY_DIR ${BUILD_DIR}
+        DEPENDS zlib
+        GIT_REPOSITORY "https://gitlab.com/libtiff/libtiff/"
+        GIT_TAG "v4.3.0"
+        UPDATE_COMMAND "" # Hack to avoid rebuild all the time on linux
+        CMAKE_ARGS
+          -Djpeg=ON
+          -Dold-jpeg=ON
+	  -Djbig=OFF # JBIG is GPL
+          -Dzlib=ON
+	  -DZLIB_INCLUDE_DIR=${TOP_BUILD_DIR}/zlib/install/include/
+	  -DZLIB_LIBRARY_RELEASE=${TOP_BUILD_DIR}/zlib/install/lib/libz.dylib
+        CMAKE_CACHE_ARGS
+          -DCMAKE_BUILD_TYPE:STRING=Release
+          -DCMAKE_VERBOSE_MAKEFILE:BOOL=OFF
+          -DCMAKE_INSTALL_MESSAGE:BOOL=LAZY
+	  -DCMAKE_INSTALL_PREFIX:STRING=${INSTALL_DIR}
+        )
+
 else()
 create_package_code("
 	file(COPY ${INSTALL_DIR}/include/ DESTINATION ${POST_INSTALL_DIR}/include/)
